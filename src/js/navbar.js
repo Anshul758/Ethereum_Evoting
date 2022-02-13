be4c2a8c8d9e50d0a9f1a1130cb18932d53e6f48
Abhishek Sharma <52832978+FABhishek@users.noreply.github.com>
2022-02-13 13:06:29 +0530

    changed front-end elements

--- src/js/navbar.js
+++ src/js/navbar.js
@@ -1,10 +1,10 @@
-let nav = document.querySelector("#navbar-custom");
+=let nav = document.querySelector("#navbar-custom");
 
 nav.innerHTML = `
 
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
 <a class="navbar-brand" href="/">
-  <img src="./img/eci.png" width="150px" loading="lazy"/>
+  <img src="./img/eci.png" width = "70px" height = "50px" loading="lazy"/>
 </a>
 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
   <span class="navbar-toggler-icon"></span>
