7009554c0257ee8583b6e83dcf05a60dccf4b04d
Abhishek Sharma <52832978+FABhishek@users.noreply.github.com>
2022-02-13 12:17:22 +0530

    Contract Updated

--- contracts/ELection.sol
+++ contracts/ELection.sol
@@ -1,56 +1,53 @@
-// SPDX-License-Identifier: GPL-3.0
-pragma solidity >=0.5.0 <0.9.0;
+pragma solidity >=0.5.0 < 0.9.0;
 
-contract Voting {
-
-    // Modeling a Candidate
+contract Election {
+    // Model a Candidate
     struct Candidate {
         uint id;
         string name;
         string party;
         uint voteCount;
-    }   
+    }
 
     // Store accounts that have voted
     mapping(address => bool) public voters;
-
-    // Store Candidates and fetch candidate
+    // Store Candidates
+    // Fetch Candidate
     mapping(uint => Candidate) public candidates;
-
-    // store candidates count
+    // Store Candidates Count
     uint public candidatesCount;
 
     // voted event
     event votedEvent (
-        uint indexed_candidateId
+        uint indexed _candidateId
     );
 
-    constructor () public {
-        addCandidate("Amit shah","Bharatiya Janata Party");
-        addCandidate("Sankar Malakar","Indian National Congress");
-        addCandidate("Saman Pathak","Communist Party Of India (Marxist)");
-        addCandidate("Amar Singh","All India Trinamool Congress");
-        addCandidate("Sudip Mandal","Bahujan Samaj Party");
+    constructor () public{
+        addCandidate("Narendra Modi","Bharatiya Janata Party");
+        addCandidate("Rahul Gandhi","Indian National Congress");
+        addCandidate("Tapan Kumar Sen","Communist Party Of India (Marxist)");
+        addCandidate("Mamata Banerjee","All India Trinamool Congress");
+        addCandidate("Mayavati","Bahujan Samaj Party");
         addCandidate("NOTA","None of the above");
     }
 
-    function addCandidate(string memory _name, string memory _party) private {
-        candidatesCount++;
-        candidates[candidatesCount] = Candidate(candidatesCount,_name,_party,0);
+    function addCandidate (string memory name,string memory party) private {
+        candidatesCount ++;
+        candidates[candidatesCount] = Candidate(candidatesCount, name,party, 0);
     }
 
     function vote (uint _candidateId) public {
-        // check that they haven't voted before
+        // require that they haven't voted before
         require(!voters[msg.sender]);
 
-        // check he/she is a valid candidate
-        require(_candidateId > 0 && _candidateId<= candidatesCount);
+        // require a valid candidate
+        require(_candidateId > 0 && _candidateId <= candidatesCount);
 
-        // record that voter has voted 
+        // record that voter has voted
         voters[msg.sender] = true;
 
-        // update candidate vote count
-        candidates[_candidateId].voteCount++;
+        // update candidate vote Count
+        candidates[_candidateId].voteCount ++;
 
         // trigger voted event
         emit votedEvent(_candidateId);
